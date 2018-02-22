30e175e75fa7b461845e330ac3ab5f0d89a7fa52
minikdj <minikdj11@gmail.com>
2018-02-22 16:06:26 -0500

    figuring out how I am going to clear the console to print the next generation

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,2 +1,12 @@
-from tkinter import *
+import time
+import os
+
+board = [[0 for x in range(20)] for y in range(20)]
+
+for x in range(len(board)):
+	for y in range(len(board)):
+		print(board[x][y], end=' ')
+	print('\n')
+time.sleep(5)
+os.system('clear')
 print('hello')
