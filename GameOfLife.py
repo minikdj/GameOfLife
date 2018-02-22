430f9b5842c9ea8d4b52552ebfadffd454bb180a
minikdj <minikdj11@gmail.com>
2018-02-22 16:55:54 -0500

    working on representing alive vs dead cells

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,15 +1,22 @@
 import time
 import os
+from random import *
 
-board = [[0 for x in range(20)] for y in range(20)]
+# O for alive, blank area for dead
+board = [['O' for x in range(20)] for y in range(10)]
 
 #clearing the terminal beforehand so everything prints evenly
 os.system('clear')
+for i in range(50):
+	for x in range(len(board)):
+		for y in range(len(board[x])):
+			if(randint(1,6) == 1):
+				board[x][y] = 'O'
+			else:
+				board[x][y] = ''
+			print(board[x][y], end=' ')
+		print('\n')
 
-for x in range(len(board)):
-	for y in range(len(board)):
-		print(board[x][y], end=' ')
-	print('\n')
-time.sleep(5)
-os.system('clear')
-print('hello')
+	time.sleep(1)
+	os.system('clear')
+
