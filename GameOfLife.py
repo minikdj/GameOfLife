3d4d2686eca34896826702073b66b0810cdd920a
minikdj <minikdj11@gmail.com>
2018-02-22 16:24:02 -0500

    clearing terminal before printing the list

--- GameOfLife.py
+++ GameOfLife.py
@@ -3,6 +3,9 @@ import os
 
 board = [[0 for x in range(20)] for y in range(20)]
 
+#clearing the terminal beforehand so everything prints evenly
+os.system('clear')
+
 for x in range(len(board)):
 	for y in range(len(board)):
 		print(board[x][y], end=' ')
