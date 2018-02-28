0a5a754d3f6e77b668f0836cd49368308d665d74
minikdj <minikdj11@gmail.com>
2018-02-28 12:13:04 -0500

    made it less hard coded

--- GameOfLife.py
+++ GameOfLife.py
@@ -6,18 +6,18 @@ extinct = False
 
 # O for alive, blank area for dead
 #board that is being printed and used to compare for the next generation
-board = [['O' for x in range(20)] for y in range(10)]
+board = [['O' for x in range(30)] for y in range(20)]
 
 #where the new values go for the new generation
-new_board = [['' for x in range(20)] for y in range(10)]
+new_board = list(board)
 
 def initialize_board():
 	for x in range(len(board)):
 		for y in range(len(board[x])):
-			if randint(1,5) == 1:
+			if randint(1,2) == 1:
 				board[x][y] = 'O'
 			else:
-				board[x][y] = ''
+				board[x][y] = ' '
 
 def draw_board():
 	#clearing the terminal beforehand so everything prints evenly
@@ -39,14 +39,14 @@ def check_top_left():
 	
 	if board[0][0] == 'O':
 		if count < 2:
-			new_board[0][0] = ''
+			new_board[0][0] = ' '
 		else:
 			new_board[0][0] = 'O'
 	else:
 		if count == 3:
 			new_board[0][0] = 'O'
 		else:
-			new_board[0][0] = ''
+			new_board[0][0] = ' '
 def check_top_right():
 	count = 0
 
@@ -59,14 +59,14 @@ def check_top_right():
 	
 	if board[0][len(board[0]) - 1] == 'O':
 		if count < 2:
-			new_board[0][len(board[0]) - 1] = ''
+			new_board[0][len(board[0]) - 1] = ' '
 		else:
 			new_board[0][len(board[0]) - 1] = 'O'
 	else:
 		if count == 3:
 			new_board[0][len(board[0]) - 1] = 'O'
 		else:
-			new_board[0][len(board[0]) - 1] = ''
+			new_board[0][len(board[0]) - 1] = ' '
 def check_top_middle():
 	for i in range(1,len(board[0]) - 1):
 		count = 0
@@ -76,7 +76,7 @@ def check_top_middle():
 		if board[1][i - 1] == 'O':
 			count += 1
 		if board[1][i] == 'O':
-			count += 1
+			count
 		if board[1][i + 1] == 'O':
 			count += 1
 		if board[0][i + 1] == 'O':
@@ -84,16 +84,16 @@ def check_top_middle():
 		
 		if board[0][i] == 'O':
 			if count < 2:
-				new_board[0][i] = ''
+				new_board[0][i] = ' '
 			elif count > 3:
-				new_board[0][i] = ''
+				new_board[0][i] = ' '
 			else:
 				new_board[0][i] = 'O'
 		else:
 			if count == 3:
 				new_board[0][i] = 'O'
 			else:
-				new_board[0][i] = ''
+				new_board[0][i] = ' '
 
 def check_bottom_left():
 	count = 0
@@ -107,14 +107,14 @@ def check_bottom_left():
 
 	if board[len(board) - 1][0] == 'O':
 		if count < 2:
-			new_board[len(board) - 1][0] = ''
+			new_board[len(board) - 1][0] = ' '
 		else:
 			new_board[len(board) - 1][0] = 'O'
 	else:
 		if count == 3:
 			new_board[len(board) - 1][0] = 'O'
 		else:
-			new_board[len(board) - 1][0] = ''	
+			new_board[len(board) - 1][0] = ' '	
 
 def check_bottom_right():
 	count = 0
@@ -128,14 +128,14 @@ def check_bottom_right():
 
 	if board[len(board) - 1][len(board[0]) - 1] == 'O':
 		if count < 2:
-			new_board[len(board) - 1][len(board[0]) - 1] = ''
+			new_board[len(board) - 1][len(board[0]) - 1] = ' '
 		else:
 			new_board[len(board) - 1][len(board[0]) - 1] = 'O'
 	else:
 		if count == 3:
 			new_board[len(board) - 1][len(board[0]) - 1] = 'O'
 		else:
-			new_board[len(board) - 1][len(board[0]) - 1] = ''
+			new_board[len(board) - 1][len(board[0]) - 1] = ' '
 
 def check_bottom_middle():
 	
@@ -155,16 +155,16 @@ def check_bottom_middle():
 	
 		if board[len(board) - 1][i] == 'O':
 			if count < 2:
-				new_board[len(board) - 1][i] = ''
+				new_board[len(board) - 1][i] = ' '
 			elif count > 3:
-				new_board[len(board) - 1][i] = ''
+				new_board[len(board) - 1][i] = ' '
 			else:
 				new_board[len(board) - 1][i] = 'O'
 		else:
 			if count == 3:
 				new_board[len(board) - 1][i] = 'O'
 			else:
-				new_board[len(board) - 1][i] = '' 	
+				new_board[len(board) - 1][i] = ' ' 	
 
 def check_top():
 	check_top_left()
@@ -189,16 +189,16 @@ def check_left():
 		
 		if board[i][0] == 'O':
 			if count < 2:
-				new_board[i][0] = ''
+				new_board[i][0] = ' '
 			elif count > 3:
-				new_board[i][0] = ''
+				new_board[i][0] = ' '
 			else:
 				new_board[i][0] = 'O'
 		else:
 			if count == 3:
 				new_board[i][0] = 'O'
 			else:
-				new_board[i][0] = ''
+				new_board[i][0] = ' '
 	
 
 def check_right():
@@ -219,16 +219,16 @@ def check_right():
 		
 		if board[i][len(board[i]) - 1] == 'O':
 			if count < 2:
-				new_board[i][len(board[i]) - 1] = ''
+				new_board[i][len(board[i]) - 1] = ' '
 			elif count > 3:
-				new_board[i][len(board[i]) - 1] = ''
+				new_board[i][len(board[i]) - 1] = ' '
 			else:
 				new_board[i][len(board[i]) - 1] = 'O'
 		else:
 			if count == 3:
 				new_board[i][len(board[i]) - 1] = 'O'
 			else:
-				new_board[i][len(board[i]) - 1] = ''
+				new_board[i][len(board[i]) - 1] = ' '
 
 
 def check_bottom():
@@ -267,16 +267,16 @@ def check_middle():
 
 			if board[i][j] == 'O':
 				if count < 2:
-					new_board[i][j] = ''
+					new_board[i][j] = ' '
 				elif count > 3:
-					new_board[i][j] = ''
+					new_board[i][j] = ' '
 				else:
 					new_board[i][j] = 'O'
 			else:
 				if count == 3:
 					new_board[i][j] = 'O'
 				else:
-					new_board[i][j] = ''
+					new_board[i][j] = ' '
 
 def update_board():
 	check_border()
@@ -301,4 +301,4 @@ while extinct == False:
 	update_board()
 	check_extinct()
 	board = list(new_board)
-	time.sleep(1)
+	time.sleep(0.1)
